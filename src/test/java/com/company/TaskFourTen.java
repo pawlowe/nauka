49757e2241550c23d6f94e42bb19addbd9561116
pawlowe <pawlowska.eliza@gmail.com>
2021-11-08 15:51:28 +0100

    task ten and eleven

---
+++ src/test/java/com/company/TaskFourTen.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import javax.swing.*;
+
+public class TaskFourTen {
+    public static void main(String[] args){
+       int number, maxValue = 0, minValue;
+       String inputNumber;
+
+        inputNumber= JOptionPane.showInputDialog("Podaj dowolną liczbę całkowitą. Jeśli chcesz zakończyć wpisz -99");
+        number = Integer.parseInt(inputNumber);
+        minValue = number;
+
+       while (number != -99){
+
+           inputNumber= JOptionPane.showInputDialog("Podaj dowolną liczbę całkowitą. Jeśli chcesz zakończyć wpisz -99");
+           number = Integer.parseInt(inputNumber);
+
+           if (number < minValue & number !=-99)
+           {
+               minValue = number;
+           }
+           else if(number > maxValue & number != -99){
+               maxValue = number;
+           }
+       }
+        System.out.println("Wartosć minimalna to " + minValue);
+        System.out.println("Wartosć maxymalna to " + maxValue);
+       System.exit(0);
+    }
+}
